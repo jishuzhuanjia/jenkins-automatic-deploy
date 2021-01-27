5f06aa6d43c80ab01c805271684455f14eb8afd0
mfcdebugger <mfcdebugger@163.com>
2021-01-27 19:39:26 +0800

    a

--- src/main/java/com/zj/test/jenkinsautomaticdeploy/controller/HelloController.java
+++ src/main/java/com/zj/test/jenkinsautomaticdeploy/controller/HelloController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     /**
-     * abcdefghijklm
+     * abcdefghijklmn
      */
     @RequestMapping("hello")
     public String test(){
