39874552327c6997d3990f364da108870df8d5b6
mfcdebugger <mfcdebugger@163.com>
2021-01-27 20:10:58 +0800

    a

--- src/main/java/com/zj/test/jenkinsautomaticdeploy/controller/HelloController.java
+++ src/main/java/com/zj/test/jenkinsautomaticdeploy/controller/HelloController.java
@@ -16,10 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     /**
-     * abcdefghijklmnopqr
+     * abcdefghijklmnopqrst
      */
     @RequestMapping("hello")
     public String test(){
-        return "恭喜你! 你已经实现了自动化部署了r";
+        return "恭喜你! 你已经实现了自动化部署了t";
     }
 }
